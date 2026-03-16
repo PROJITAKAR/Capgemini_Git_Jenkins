23abd70b022b7e6bf986199b2dafa03e02acf512
PROJITA KAR <projita2003kar@gmail.com>
2026-03-16 10:43:05 +0530

    Add jenkins4Test.java file

---
+++ src/test/java/jenkinsProgram/Jenkins4Test.java
@@ -0,0 +1,21 @@
+package jenkinsProgram;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Jenkins4Test {
+	@Test
+	public void sample4Test()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://www.flipkart.com/");
+		System.out.println("Title: "+ driver.getTitle());
+		driver.quit();
+
+	}
+}
